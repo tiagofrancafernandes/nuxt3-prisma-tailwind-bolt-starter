da70b47a25ab1e8c4206e68d9cc0f67e44f81ee7
Develop AWS Server <develop@digitalpixel.com.br>
2025-05-27 19:19:16 +0000

    serve scripts 	new file:   serve-node.sh 	new file:   serve-pm2.sh

---
+++ serve-pm2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+## Inicia a aplicação com PM2 em modo cluster para melhor performance
+## O modo cluster aproveita múltiplos núcleos de CPU
+pm2-runtime start .output/server/index.mjs --name "nuxt-app" -i max
+
+### Alternativa sem PM2 Via nodejs. Eu testei e gostei
+# node .output/server/index.mjs
