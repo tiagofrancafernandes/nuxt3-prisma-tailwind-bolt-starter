7747b4f8b539c44e871c7b17d36dda7445408b02
Tiago França <github@tiagofranca.com>
2025-05-27 13:34:00 -0300

    Reset project changes

--- server/api/public/posts/index.ts
+++ server/api/public/posts/index.ts
@@ -1,36 +1,27 @@
-import { PrismaClient } from '@prisma/client'
-import { prisma } from '~/server/utils/prisma'
+import { prisma } from '~/server/utils/prisma';
 
 export default defineEventHandler(async (event) => {
-    try {
-        const method = event.method;
+    const method = event.method;
 
-        switch (method) {
-            case 'GET':
-                return await prisma.post.findMany({
-                    orderBy: { createdAt: 'desc' },
-                });
-            case 'POST':
-                const post = await readBody(event);
-                return await prisma.post.create({
-                    data: {
-                        title: post.title,
-                        description: post.description,
-                        content: post.content,
-                        slug: post.title.toLowerCase().replace(/\s+/g, '-'),
-                    },
-                });
-            default:
-                throw createError({
-                    statusCode: 405,
-                    message: 'Method not allowed',
-                });
-        }
-    } catch (error) {
-        console.error('API Error:', error);
-        throw createError({
-            statusCode: 500,
-            message: 'Internal server error',
-        });
+    switch (method) {
+        case 'GET':
+            return await prisma.posts.findMany({
+                orderBy: { createdAt: 'desc' },
+            });
+        case 'POST':
+            const post = await readBody(event);
+            return await prisma.posts.create({
+                data: {
+                    title: post.title,
+                    description: post.description,
+                    content: post.content,
+                    slug: post.title.toLowerCase().replace(/\s+/g, '-'),
+                },
+            });
+        default:
+            throw createError({
+                statusCode: 405,
+                message: 'Method not allowed',
+            });
     }
 });
